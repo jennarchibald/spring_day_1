217afe1058ed40330ebd74edab9776225c3068f9
jennarchibald <jennarchibald91@gmail.com>
2019-05-20 14:30:01 +0100

    lab

---
+++ src/main/java/com/codeclan/example/pirateservice/models/Pirate.java
@@ -0,0 +1,64 @@
+package com.codeclan.example.pirateservice.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pirates")
+public class Pirate {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="age")
+    private int age;
+
+    public Pirate(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Pirate(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
